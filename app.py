b783ae2033010b8e1d5d4f452579044947078c8b
MeYar <107683112+MeYarWebAp@users.noreply.github.com>
2023-01-03 02:33:56 +0330

    Update app.py

--- app.py
+++ app.py
@@ -170,9 +170,9 @@ mystyle = '''
 st.markdown(mystyle, unsafe_allow_html=True)
 
 # Generate Three equal columns
-c1, c2, c3 = st.columns((1, 1, 1))
+c1, c2 = st.columns((1, 1))
 
-with c1:
+with c2:
     st.info("""معیار، بر مبنای اعتماد بوده و کوششی برای محافظت از خانواده و ارزش هاست""")
 #st.markdown("<h5 style='text-align: center;'> معیار، بر مبنای اعتماد بوده و کوششی برای محافظت از خانواده و ارزش هاست</h5>", unsafe_allow_html=True )
 #st.info("معیار، بر مبنای اعتماد بوده و کوششی برای محافظت از خانواده و ارزش هاست")
