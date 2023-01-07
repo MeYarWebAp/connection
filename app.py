1c83d252de4148e1d6031e5eaec1b4ec726c9a2d
MeYar <107683112+MeYarWebAp@users.noreply.github.com>
2023-01-07 16:25:40 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1675,11 +1675,14 @@ if (num00==sp):
 
         return image_tf/image_tf.max()  # value fall between 0 and 1
     numm=random.randint(1000000001, 9999999999)
+    if ('number' not in st.session_state):
+        st.session_state['number'] = numm
     #copynumm=numm
     image = create_captcha(str(numm), shear=0)
     st.image(image=image, width=400)
     nummm = st.number_input('لطفا عدد موجود در تصویر را وارد کنید',key=63529994252525,step=1) 
-    
+    if nummm==st.session_state['number']:
+        st.stop()
     
     p = st.button('معرفی کن', key="10478502")
     
