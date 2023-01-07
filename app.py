728a4e08b102f5d8ff535e7f69209be34ea1774d
MeYar <107683112+MeYarWebAp@users.noreply.github.com>
2023-01-07 16:37:02 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1682,7 +1682,7 @@ if (num00==sp):
     st.image(image=image, width=400)
     nummm = st.number_input('لطفا عدد موجود در تصویر را وارد کنید',key=63529994252525,step=1) 
     st.write(st.session_state['number'])
-    if nummm==st.session_state['number']:
+    if not nummm==st.session_state['number']:
         st.stop()
     
     p = st.button('معرفی کن', key="10478502")
